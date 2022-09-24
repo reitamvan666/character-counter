5a523899de91ca19643f2ac0b84817d5d548cfdc
reitamvan666 <113830975+reitamvan666@users.noreply.github.com>
2022-09-24 15:18:36 +0700

    Update main.js

--- main.js
+++ main.js
@@ -2,13 +2,18 @@ const input = document.querySelector(".input");
 const counter = document.querySelector(".counter");
 const btnReset = document.querySelector(".btnReset");
 
+//this will change the 0 on counter textcontent into input value length
 function calculateInput() {
   counter.textContent = input.value.length;
 }
 
+
+// this will reset the input value into nothing and counter back to 0
 function resetCounter() {
   input.value = "";
   counter.textContent = 0;
 }
+
+//adding an event listner
 input.addEventListener("keyup", calculateInput);
 btnReset.addEventListener("click", resetCounter);
